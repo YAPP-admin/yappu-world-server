7f05e72bda0040cb16ffe11c6238f4315568fc4c
devmizz <garamminchan@gmail.com>
2025-01-26 16:30:55 +0900

    feat: 직군 데이터 API

--- src/main/kotlin/co/yappuworld/global/security/SecurityPathMatchersManager.kt
+++ src/main/kotlin/co/yappuworld/global/security/SecurityPathMatchersManager.kt
@@ -1,5 +1,6 @@
 package co.yappuworld.global.security
 
+import org.springframework.http.HttpMethod.GET
 import org.springframework.http.HttpMethod.POST
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher
 import org.springframework.security.web.util.matcher.RequestMatchers
@@ -10,7 +11,9 @@ object SecurityPathMatchersManager {
         // swagger
         antMatcher("/swagger-ui/**"),
         antMatcher("/v3/api-docs/**"),
-        antMatcher(POST, "/v1/auth/sign-up")
+        // api
+        antMatcher(POST, "/v1/auth/sign-up"),
+        antMatcher(GET, "/v1/positions")
     )
 
     val adminMatchers = RequestMatchers.anyOf(
--- src/main/kotlin/co/yappuworld/user/presentation/vo/PositionApiType.kt
+++ src/main/kotlin/co/yappuworld/user/presentation/vo/PositionApiType.kt
@@ -2,13 +2,15 @@ package co.yappuworld.user.presentation.vo
 
 import co.yappuworld.user.domain.vo.Position
 
-enum class PositionApiType {
-    PM,
-    DESIGN,
-    WEB,
-    ANDROID,
-    IOS,
-    SERVER;
+enum class PositionApiType(
+    val label: String
+) {
+    PM("PM"),
+    DESIGN("Design"),
+    WEB("Web"),
+    ANDROID("Android"),
+    IOS("iOS"),
+    SERVER("Server");
 
     fun toDomainType(): Position {
         return when (this) {
