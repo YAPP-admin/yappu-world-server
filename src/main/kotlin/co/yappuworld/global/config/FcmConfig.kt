dcfbbded993cbf376e73d030a42087b123d0adad
devmizz <garamminchan@gmail.com>
2025-02-03 01:30:33 +0900

    feat: 환경별 FcmClient

--- src/main/kotlin/co/yappuworld/global/config/FcmConfig.kt
+++ src/main/kotlin/co/yappuworld/global/config/FcmConfig.kt
@@ -7,8 +7,10 @@ import com.google.firebase.FirebaseOptions
 import com.google.firebase.messaging.FirebaseMessaging
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 
 @Configuration
+@Profile("!test")
 class FcmConfig(
     private val fcmProperty: FcmProperty
 ) {
