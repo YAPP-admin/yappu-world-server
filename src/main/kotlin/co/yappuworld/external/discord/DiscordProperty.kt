7ac7e0aebdacf553e956ba17011fc27787d9b1eb
devmizz <garamminchan@gmail.com>
2025-02-11 01:36:50 +0900

    feat: 별의 별짓 다 함 12

--- src/main/kotlin/co/yappuworld/external/discord/DiscordProperty.kt
+++ src/main/kotlin/co/yappuworld/external/discord/DiscordProperty.kt
@@ -4,5 +4,5 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "discord")
 data class DiscordProperty(
-    val webhookUri: String = ""
+    val webhook: String
 )
