c0c6005ca3e7d91812df46ab4ca73c8fbb925b7c
devmizz <garamminchan@gmail.com>
2025-02-10 18:58:38 +0900

    fix: 프로필 조회 API 수정

--- src/main/kotlin/co/yappuworld/user/application/dto/response/UserProfileAppResponseDto.kt
+++ src/main/kotlin/co/yappuworld/user/application/dto/response/UserProfileAppResponseDto.kt
@@ -2,12 +2,13 @@ package co.yappuworld.user.application.dto.response
 
 import co.yappuworld.user.domain.model.ActivityUnit
 import co.yappuworld.user.domain.model.User
+import co.yappuworld.user.domain.vo.UserRole
 import java.util.UUID
 
 data class UserProfileAppResponseDto(
     val id: UUID,
     val name: String,
-    val role: String,
+    val role: UserRole,
     val activityUnits: List<ActivityUnitAppResponseDto>
 ) {
 
@@ -19,7 +20,7 @@ data class UserProfileAppResponseDto(
             return UserProfileAppResponseDto(
                 user.id,
                 user.name,
-                user.role.label,
+                user.role,
                 activityUnits.map { ActivityUnitAppResponseDto.of(it) }
             )
         }
