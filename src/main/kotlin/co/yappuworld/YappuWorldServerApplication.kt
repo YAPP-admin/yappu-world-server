23dab7182ba989a1ce331b60b887573607333db2
devmizz <garamminchan@gmail.com>
2025-02-13 13:05:04 +0900

    feat: 빌드 시 비밀번호 자동 생성 OFF

--- src/main/kotlin/co/yappuworld/YappuWorldServerApplication.kt
+++ src/main/kotlin/co/yappuworld/YappuWorldServerApplication.kt
@@ -1,9 +1,10 @@
 package co.yappuworld
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [UserDetailsServiceAutoConfiguration::class])
 class YappuWorldServerApplication
 
 fun main(args: Array<String>) {
