317dc968d99353af5a77afda70930e83c292a8fc
민찬기 <garamminchan@gmail.com>
2025-02-26 15:25:42 +0900

    docs: 스웨거에 공통 에러 노출 (#38)

---
+++ src/main/kotlin/co/yappuworld/global/config/SwaggerCommonResponses.kt
@@ -0,0 +1,92 @@
+package co.yappuworld.global.config
+
+import io.swagger.v3.oas.models.examples.Example
+import io.swagger.v3.oas.models.media.Content
+import io.swagger.v3.oas.models.media.MediaType
+import io.swagger.v3.oas.models.responses.ApiResponse
+
+object SwaggerCommonResponses {
+    val values = listOf(
+        SwaggerCommonResponse(
+            "500",
+            ApiResponse().apply {
+                description = "예기치 못한 서버 에러"
+                content = Content().apply {
+                    addMediaType(
+                        "application/json",
+                        MediaType().apply {
+                            examples = mapOf(
+                                "예기치 못한 에러" to Example().apply {
+                                    value = """
+                                        {
+                                            "isSuccess": "false",
+                                            "message": "예기치 못한 에러가 발생했습니다. 서버 관리자에게 문의해주세요.",
+                                            "errorCode": "COM_0001"
+                                        }
+                                    """.trimIndent()
+                                }
+                            )
+                        }
+                    )
+                }
+            }
+        ),
+        SwaggerCommonResponse(
+            "400",
+            ApiResponse().apply {
+                description = "잘못된 파라미터 요청"
+                content = Content().apply {
+                    addMediaType(
+                        "application/json",
+                        MediaType().apply {
+                            examples = mapOf(
+                                "잘못된 파라미터 요청" to Example().apply {
+                                    value = """
+                                        {
+                                            "isSuccess": "false",
+                                            "message": "요청된 인자에 잘못된 값이 있습니다.",
+                                            "errorCode": "COM_0002"
+                                        }
+                                    """.trimIndent()
+                                }
+                            )
+                        }
+                    )
+                }
+            }
+        ),
+        SwaggerCommonResponse(
+            "403",
+            ApiResponse().apply {
+                description = "인증 실패"
+                content = Content().apply {
+                    addMediaType(
+                        "application/json",
+                        MediaType().apply {
+                            examples = mapOf(
+                                "토큰 만료" to Example().apply {
+                                    value = """
+                                        {
+                                            "isSuccess": "false",
+                                            "message": "만료된 토큰입니다.",
+                                            "errorCode": "TKN_0001"
+                                        }
+                                    """.trimIndent()
+                                },
+                                "비정상 토큰" to Example().apply {
+                                    value = """
+                                        {
+                                            "isSuccess": "false",
+                                            "message": "비정상 토큰입니다.",
+                                            "errorCode": "TKN_0002"
+                                        }
+                                    """.trimIndent()
+                                }
+                            )
+                        }
+                    )
+                }
+            }
+        )
+    )
+}
--- src/main/kotlin/co/yappuworld/global/config/SwaggerConfig.kt
+++ src/main/kotlin/co/yappuworld/global/config/SwaggerConfig.kt
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.security.SecurityRequirement
 import io.swagger.v3.oas.models.security.SecurityScheme
+import org.springdoc.core.customizers.OpenApiCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -19,6 +20,20 @@ class SwaggerConfig {
         return OpenAPI().info(getInfo())
             .addSecurityItem(getSecurityRequirement())
             .components(Components().addSecuritySchemes(securitySchemeName, getSecurityScheme()))
+    }
+
+    @Bean
+    fun customGlobalResponses(): OpenApiCustomizer {
+        return OpenApiCustomizer { openApi ->
+            openApi.paths.values.forEach { item ->
+                item.readOperations().forEach { operation ->
+                    val responses = operation.responses
+                    SwaggerCommonResponses.values.forEach { value ->
+                        responses.addApiResponse(value.code, value.apiResponse)
+                    }
+                }
+            }
+        }
     }
 
     private fun getInfo(): Info {
