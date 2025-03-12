556baab6c7208f7a9994568cc70a781789cea08c
devmizz <garamminchan@gmail.com>
2025-03-13 00:01:52 +0900

    test: logging 추가

--- src/main/kotlin/co/yappuworld/external/fcm/FcmTestController.kt
+++ src/main/kotlin/co/yappuworld/external/fcm/FcmTestController.kt
@@ -3,7 +3,9 @@ package co.yappuworld.external.fcm
 import com.google.firebase.messaging.ApnsConfig
 import com.google.firebase.messaging.Aps
 import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.FirebaseMessagingException
 import com.google.firebase.messaging.Message
+import io.github.oshai.kotlinlogging.KotlinLogging
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.context.annotation.Profile
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
+
+private val logger = KotlinLogging.logger { }
 
 @Profile("local", "dev")
 @Tag(name = "FCM 테스트용")
@@ -41,13 +45,23 @@ class FcmTestController(
     fun sendNotificationOnlyData(
         @RequestBody request: FcmOnlyDataRequest
     ) {
-        firebaseMessaging.send(
-            Message
-                .builder()
-                .setToken(request.token)
-                .putAllData(request.toData())
-                .build()
-        )
+        try {
+            firebaseMessaging.send(
+                Message
+                    .builder()
+                    .setToken(request.token)
+                    .putAllData(request.toData())
+                    .build()
+            )
+        } catch (e: FirebaseMessagingException) {
+            logger.warn {
+                """
+                    푸시 알림 전송에 실패했습니다.
+                    실패 사유: ${e.messagingErrorCode.name}
+                    fcm_token: ${request.token}
+                """.trimIndent()
+            }
+        }
     }
 
     @Operation(summary = "Content Available 설정된 FCM 메세지")
@@ -55,20 +69,30 @@ class FcmTestController(
     fun sendFcmWithContentAvailable(
         @RequestBody request: FcmOnlyDataRequest
     ) {
-        firebaseMessaging.send(
-            Message
-                .builder()
-                .setApnsConfig(
-                    ApnsConfig
-                        .builder()
-                        .setAps(
-                            Aps
-                                .builder()
-                                .setContentAvailable(true)
-                                .build()
-                        ).build()
-                ).putAllData(request.toData())
-                .build()
-        )
+        try {
+            firebaseMessaging.send(
+                Message
+                    .builder()
+                    .setApnsConfig(
+                        ApnsConfig
+                            .builder()
+                            .setAps(
+                                Aps
+                                    .builder()
+                                    .setContentAvailable(true)
+                                    .build()
+                            ).build()
+                    ).putAllData(request.toData())
+                    .build()
+            )
+        } catch (e: FirebaseMessagingException) {
+            logger.warn {
+                """
+                    푸시 알림 전송에 실패했습니다.
+                    실패 사유: ${e.messagingErrorCode.name}
+                    fcm_token: ${request.token}
+                """.trimIndent()
+            }
+        }
     }
 }
