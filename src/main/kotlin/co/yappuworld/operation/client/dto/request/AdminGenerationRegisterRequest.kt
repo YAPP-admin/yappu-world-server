e5c8e2b7ceb3f825e5be456d19e81e6a2cf778b4
devmizz <garamminchan@gmail.com>
2025-04-05 03:21:13 +0900

    fix: 기수 생성 시 시작일 종료일을 Nullable 하게 처리

--- src/main/kotlin/co/yappuworld/operation/client/dto/request/AdminGenerationRegisterRequest.kt
+++ src/main/kotlin/co/yappuworld/operation/client/dto/request/AdminGenerationRegisterRequest.kt
@@ -11,11 +11,9 @@ data class AdminGenerationRegisterRequest(
     @field:Min(value = 1L, message = "기수는 1 이상이어야 합니다.")
     val generation: Int,
     @Schema(description = "시작일")
-    @field:NotNull(message = "시작일은 필수 값입니다.")
-    val startDate: LocalDate,
+    val startDate: LocalDate?,
     @Schema(description = "종료일")
-    @field:NotNull(message = "종료일은 필수 값입니다.")
-    val endDate: LocalDate,
+    val endDate: LocalDate?,
     @Schema(description = "활동 중 여부")
     @field:NotNull(message = "활동 중 여부는 필수 값입니다.")
     val isActive: Boolean
