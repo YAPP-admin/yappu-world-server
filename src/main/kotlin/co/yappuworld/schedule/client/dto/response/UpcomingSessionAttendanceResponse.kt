deda5ac0014249e5afbc0b08650499eb5db0aef8
devmizz <garamminchan@gmail.com>
2025-04-29 18:52:46 +0900

    fix: 남은 일자 수, 요일 필드를 일정 응답에 추가

--- src/main/kotlin/co/yappuworld/schedule/client/dto/response/UpcomingSessionAttendanceResponse.kt
+++ src/main/kotlin/co/yappuworld/schedule/client/dto/response/UpcomingSessionAttendanceResponse.kt
@@ -1,5 +1,6 @@
 package co.yappuworld.schedule.client.dto.response
 
+import co.yappuworld.global.util.TimeUtils.korean
 import co.yappuworld.schedule.domain.AttendanceEntity
 import co.yappuworld.schedule.domain.SessionEntity
 import co.yappuworld.user.domain.vo.UserRole
@@ -8,6 +9,7 @@ import io.swagger.v3.oas.annotations.media.Schema
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.LocalTime
+import java.time.temporal.ChronoUnit
 import java.util.UUID
 
 data class UpcomingSessionAttendanceResponse(
@@ -15,10 +17,29 @@ data class UpcomingSessionAttendanceResponse(
     val sessionId: UUID,
     @Schema(description = "세션 이름")
     val name: String,
-    @Schema(description = "세션 일자")
-    val date: LocalDate,
-    @Schema(description = "세션 시간")
-    val time: LocalTime?,
+    @Schema(description = "세션 시작 일자")
+    val startDate: LocalDate,
+    @Schema(description = "세션 시작 요일")
+    val startDayOfWeek: String,
+    @Schema(description = "세션 종료 일자")
+    val endDate: LocalDate,
+    @Schema(description = "세션 종료 요일")
+    val endDayOfWeek: String,
+    @Schema(description = "세션 시작 시간")
+    val startTime: LocalTime?,
+    @Schema(description = "세션 종료 시간")
+    val endTime: LocalTime?,
+    @Schema(description = "장소")
+    val place: String?,
+    @Schema(
+        description = """
+            세션 시작일 기준 상대 날짜. D-N 혹은 D+N 으로 표시되는 값.
+            ex) -2(D-2): 세션 시작일 기준 2일 전
+            ex) 0(D-0, D+0): 세션 당일
+            ex) +3(D+3): 세션 시작일 기준 3일 후
+        """
+    )
+    val relativeDays: Int,
     @Schema(
         description = """
             현재 출석이 가능한지 여부
@@ -50,8 +71,14 @@ data class UpcomingSessionAttendanceResponse(
             return UpcomingSessionAttendanceResponse(
                 sessionId = session.id,
                 name = session.name,
-                date = session.date,
-                time = session.time,
+                startDate = session.date,
+                startDayOfWeek = session.date.dayOfWeek.korean(),
+                endDate = session.endDate,
+                endDayOfWeek = session.endDate.dayOfWeek.korean(),
+                startTime = session.time,
+                endTime = session.endTime,
+                place = session.place,
+                relativeDays = ChronoUnit.DAYS.between(session.date, now.toLocalDate()).toInt(),
                 canCheckIn = canCheckIn,
                 status = attendance?.status?.label
             )
