2deec46336b464c62b95b72e1cfe8048036e3a86
민찬기 <garamminchan@gmail.com>
2025-05-06 15:40:19 +0900

    fix: 임박한 세션 조회 쿼리 개선 (#132)

--- src/main/kotlin/co/yappuworld/schedule/infrastructure/SessionFindService.kt
+++ src/main/kotlin/co/yappuworld/schedule/infrastructure/SessionFindService.kt
@@ -35,7 +35,13 @@ class SessionFindService(
                     .where(
                         and(
                             path(SessionEntity::generation).equal(activeGeneration),
-                            path(SessionEntity::date).greaterThanOrEqualTo(now.toLocalDate())
+                            or(
+                                path(SessionEntity::endDate).greaterThan(now.toLocalDate()),
+                                and(
+                                    path(SessionEntity::endDate).equal(now.toLocalDate()),
+                                    path(SessionEntity::endTime).greaterThan(now.toLocalTime())
+                                )
+                            )
                         )
                     ).orderBy(path(SessionEntity::date).asc())
             }.singleOrNull()
