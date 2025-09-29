a6473a5acd402e4e238f21f9fd1a273c6b7239e4
eeun1225 <eeun9108@gmail.com>
2025-09-29 22:43:05 +0900

    style: KtLint 포맷 적용 (#172)

--- src/main/kotlin/co/yappuworld/external/messenger/MessengerClient.kt
+++ src/main/kotlin/co/yappuworld/external/messenger/MessengerClient.kt
@@ -3,6 +3,8 @@ package co.yappuworld.external.messenger
 import co.yappuworld.external.messenger.dto.DiscordEmbed
 
 interface MessengerClient {
+
     fun send(content: String)
+
     fun send(embed: DiscordEmbed)
 }
