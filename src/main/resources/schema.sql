6eedf872eca69d840728f23bcfed501bf9c9354d
eeun1225 <eeun9108@gmail.com>
2025-10-31 17:51:09 +0900

    fix: sql문 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -140,35 +140,35 @@ create table late_passes
     count      int NOT NULL DEFAULT 0
 );
 
-drop table if exists services;
+DROP TABLE IF EXISTS services;
 CREATE TABLE services
 (
     id              binary(16) PRIMARY KEY,
     created_at      datetime(6),
     updated_at      datetime(6),
-    has_app         boolean NOT NULL DEFAULT FALSE,
-    has_web         boolean NOT NULL DEFAULT FALSE,
+    has_app         tinyint(1) NOT NULL DEFAULT 0,
+    has_web         tinyint(1) NOT NULL DEFAULT 0,
     name            varchar(255) DEFAULT NULL,
     service_links   json DEFAULT NULL
-)
+);
 
-drop table if exists teams;
+DROP TABLE IF EXISTS teams;
 CREATE TABLE teams
 (
-    id              binary(16) PRIMARY KEY,
-    created_at      datetime(6),
-    updated_at      datetime(6),
-    generation      int NOT NULL,
-    name            varchar(255) NOT NULL,
-    service_id      binary(16) DEFAULT NULL
-)
+    id          binary(16) PRIMARY KEY,
+    created_at  datetime(6),
+    updated_at  datetime(6),
+    generation  int NOT NULL,
+    name        varchar(255) NOT NULL,
+    service_id  binary(16) DEFAULT NULL
+);
 
-drop table if exists team_members;
+DROP TABLE IF EXISTS team_members;
 CREATE TABLE team_members
 (
-    id                  binary(16) PRIMARY KEY,
-    created_at          datetime(6),
-    updated_at          datetime(6),
-    activity_unit_id    binary(16),
-    team_id             binary(16)
-)
+    id               binary(16) PRIMARY KEY,
+    created_at       datetime(6),
+    updated_at       datetime(6),
+    activity_unit_id binary(16),
+    team_id          binary(16)
+);
