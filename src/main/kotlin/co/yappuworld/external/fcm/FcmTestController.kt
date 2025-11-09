a7ea56785e9f40f79da9eba4daf597a96b7ad703
devmizz <garamminchan@gmail.com>
2025-11-09 09:45:08 +0900

    fix: 불필요한 의존성 제거

--- src/main/kotlin/co/yappuworld/external/fcm/FcmTestController.kt
+++ src/main/kotlin/co/yappuworld/external/fcm/FcmTestController.kt
@@ -5,7 +5,6 @@ import io.github.oshai.kotlinlogging.KotlinLogging
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.context.annotation.Profile
-import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -16,8 +15,7 @@ private val logger = KotlinLogging.logger { }
 @Tag(name = "FCM 테스트용", description = "_")
 @RestController
 class FcmTestController(
-    private val fcmClient: FcmClient,
-    private val jdbcTemplate: JdbcTemplate
+    private val fcmClient: FcmClient
 ) {
 
     data class SingleFcm(
