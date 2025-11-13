c590ce8fda906154f45111b5ca02f9bfc511ccb0
eeun1225 <eeun9108@gmail.com>
2025-11-13 18:22:45 +0900

    fix: ::id를 ::getId로 변경 (private 필드 접근 불가)

--- src/main/kotlin/co/yappuworld/team/infrastructure/CustomTeamDsl.kt
+++ src/main/kotlin/co/yappuworld/team/infrastructure/CustomTeamDsl.kt
@@ -37,10 +37,10 @@ CustomTeamDsl : Jpql() {
 
     fun selectTeamWithService(): SelectQueryFromStep<TeamWithServiceDto> =
         selectNew<TeamWithServiceDto>(
-            path(TeamEntity::id),
+            path(TeamEntity::getId),
             path(TeamEntity::name),
             path(TeamEntity::generation),
-            path(TeamServiceEntity::id),
+            path(TeamServiceEntity::getId),
             path(TeamServiceEntity::name),
             path(TeamServiceEntity::hasApp),
             path(TeamServiceEntity::hasWeb),
@@ -49,7 +49,7 @@ CustomTeamDsl : Jpql() {
 
     fun selectTeamMemberDetail(): SelectQueryFromStep<TeamMemberDetailDto> =
         selectNew<TeamMemberDetailDto>(
-            path(TeamMemberEntity::activityUnit).path(ActivityUnitEntity::id),
+            path(TeamMemberEntity::activityUnit).path(ActivityUnitEntity::getId),
             path(TeamMemberEntity::activityUnit).path(ActivityUnitEntity::generation),
             path(TeamMemberEntity::activityUnit).path(ActivityUnitEntity::position),
             path(UserEntity::name)
