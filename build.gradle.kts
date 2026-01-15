d4cfa501572d08858a97da4c404ca8cd062eacd4
devmizz <garamminchan@gmail.com>
2026-01-15 23:34:01 +0900

    infra: h2 기반으로 ci 환경 구축

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,7 @@ dependencies {
     runtimeOnly("com.mysql:mysql-connector-j")
     runtimeOnly("com.oracle.database.jdbc:ojdbc11")
     runtimeOnly("com.oracle.database.security:oraclepki:23.5.0.24.07")
+    runtimeOnly("com.h2database:h2")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("com.linecorp.kotlin-jdsl:jpql-dsl:3.5.5")
     implementation("com.linecorp.kotlin-jdsl:jpql-render:3.5.5")
