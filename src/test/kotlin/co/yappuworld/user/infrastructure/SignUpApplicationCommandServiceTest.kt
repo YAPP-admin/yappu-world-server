19d24e06d06fc466fcd12782d1cf8794383a8a9c
eeun1225 <eeun9108@gmail.com>
2026-02-05 21:41:56 +0900

    feat: 가입신청서 테이블 분리 및 컬럼 추가 (#202)

--- src/test/kotlin/co/yappuworld/user/infrastructure/SignUpApplicationCommandServiceTest.kt
+++ src/test/kotlin/co/yappuworld/user/infrastructure/SignUpApplicationCommandServiceTest.kt
@@ -2,15 +2,18 @@ package co.yappuworld.user.infrastructure
 
 import co.yappuworld.support.environment.CustomDataJpaTestFeatureSpec
 import co.yappuworld.support.fixture.UserFixture.getSignUpApplicationEntityFixture
+import co.yappuworld.user.infrastructure.jpa.SignUpApplicationActivityUnitRepository
 import co.yappuworld.user.infrastructure.jpa.SignUpApplicationRepository
 import io.kotest.matchers.nulls.shouldNotBeNull
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.repository.findByIdOrNull
 
 class SignUpApplicationCommandServiceTest @Autowired constructor(
-    private val signUpApplicationRepository: SignUpApplicationRepository
+    private val signUpApplicationRepository: SignUpApplicationRepository,
+    private val signUpApplicationActivityUnitRepository: SignUpApplicationActivityUnitRepository
 ) : CustomDataJpaTestFeatureSpec({
-        val signUpApplicationCommandService = SignUpApplicationCommandService(signUpApplicationRepository)
+        val signUpApplicationCommandService =
+            SignUpApplicationCommandService(signUpApplicationRepository, signUpApplicationActivityUnitRepository)
 
         feature("회원 가입 신청서 제출") {
 
